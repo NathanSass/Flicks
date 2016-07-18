4ca73923a5dc6bf10d6d27c3c3f937f898c25299
Nathan Sass <nsass711@gmail.com>
2016-07-18 13:21:28 -0700

    Able to create movie objects TODO display movie info

--- app/src/main/java/com/nathansass/flicks/MainActivity.java
+++ app/src/main/java/com/nathansass/flicks/MainActivity.java
@@ -1,16 +1,24 @@
 package com.nathansass.flicks;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 import com.loopj.android.http.AsyncHttpClient;
 import com.loopj.android.http.JsonHttpResponseHandler;
+import com.nathansass.flicks.models.Movie;
 
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.util.ArrayList;
 
 import cz.msebera.android.httpclient.Header;
 
 public class MainActivity extends AppCompatActivity {
+
+    ArrayList<Movie> movies;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,11 +28,19 @@ public class MainActivity extends AppCompatActivity {
         String url = "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed";
 
         AsyncHttpClient client = new AsyncHttpClient();
-
         client.get(url, new JsonHttpResponseHandler() {
             @Override
             public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
-                super.onSuccess(statusCode, headers, response);
+                JSONArray movieJsonResults = null;
+                try {
+                    movieJsonResults = response.getJSONArray("results");
+                    movies = Movie.fromJSONArray(movieJsonResults);
+                    Log.d("DEBUG", movies.toString());
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
             }
 
             @Override
