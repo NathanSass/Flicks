34f420a97c16180234e661f3fa952989d55fcaff
Nathan Sass <nsass711@gmail.com>
2016-07-20 14:17:13 -0700

    Implemeneted view holder pattern

--- app/src/main/java/com/nathansass/flicks/adapters/MovieArrayAdapter.java
+++ app/src/main/java/com/nathansass/flicks/adapters/MovieArrayAdapter.java
@@ -20,6 +20,14 @@ import java.util.List;
  */
 public class MovieArrayAdapter extends ArrayAdapter<Movie> {
 
+//    @BindView(R.id.ivMovieImage) ImageView ivImage;
+
+    public static class ViewHolder {
+        TextView tvTitle;
+        TextView tvOverview;
+        ImageView ivImage;
+    }
+
     public MovieArrayAdapter(Context context, List<Movie> movies) {
         super(context, android.R.layout.simple_list_item_1, movies);
     }
@@ -28,17 +36,27 @@ public class MovieArrayAdapter extends ArrayAdapter<Movie> {
     public View getView(int position, View convertView, ViewGroup parent) {
         Movie movie = getItem(position);
 
+
+        ViewHolder viewHolder;
+
         if (convertView == null) {
+            viewHolder = new ViewHolder();
+
             LayoutInflater inflater = LayoutInflater.from(getContext());
-            convertView = inflater.inflate(R.layout.item_movie,parent,false);
+            convertView = inflater.inflate(R.layout.item_movie, parent,false);
+
+            viewHolder.tvTitle = (TextView) convertView.findViewById(R.id.tvTitle);
+            viewHolder.tvOverview = (TextView) convertView.findViewById(R.id.tvOverview);
+            viewHolder.ivImage = (ImageView) convertView.findViewById(R.id.ivMovieImage);
+            viewHolder.ivImage.setImageResource(0);
+
+            convertView.setTag(viewHolder);
+
+        } else {
+            viewHolder = (ViewHolder) convertView.getTag();
         }
 
-        ImageView ivImage = (ImageView) convertView.findViewById(R.id.ivMovieImage);
 
-        ivImage.setImageResource(0);
-
-        TextView tvTitle = (TextView) convertView.findViewById(R.id.tvTitle);
-        TextView tvOverview = (TextView) convertView.findViewById(R.id.tvOverview);
 
         String movieImage = "";
         int movieImagePlaceholder = 0;
@@ -55,10 +73,10 @@ public class MovieArrayAdapter extends ArrayAdapter<Movie> {
 
         Picasso.with(getContext()).load(movieImage).fit()
                 .placeholder(movieImagePlaceholder)
-                .into(ivImage);
+                .into(viewHolder.ivImage);
 
-        tvTitle.setText(movie.getOriginalTitle());
-        tvOverview.setText(movie.getOverview());
+        viewHolder.tvTitle.setText(movie.getOriginalTitle());
+        viewHolder.tvOverview.setText(movie.getOverview());
 
         return convertView;
     }
