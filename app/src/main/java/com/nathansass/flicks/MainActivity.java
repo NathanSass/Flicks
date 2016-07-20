5c9602100b83d0ff593e277636fb0edbbadea311
Nathan Sass <nsass711@gmail.com>
2016-07-19 22:10:27 -0700

    implemented pull to refresh

--- app/src/main/java/com/nathansass/flicks/MainActivity.java
+++ app/src/main/java/com/nathansass/flicks/MainActivity.java
@@ -1,6 +1,7 @@
 package com.nathansass.flicks;
 
 import android.os.Bundle;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ListView;
 
@@ -22,29 +23,54 @@ public class MainActivity extends AppCompatActivity {
     ArrayList<Movie> movies;
     MovieArrayAdapter movieAdapter;
     ListView lvItems;
+    private SwipeRefreshLayout swipeContainer;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        swipeContainer = (SwipeRefreshLayout) findViewById(R.id.swipeContainer);
+
+
         lvItems = (ListView) findViewById(R.id.lvMovies);
         movies = new ArrayList<>();
         movieAdapter = new MovieArrayAdapter(this, movies);
         lvItems.setAdapter(movieAdapter);
 
+        fetchMovieAsync();
+
+        swipeContainer.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                fetchMovieAsync();
+            }
+        });
+
+        swipeContainer.setColorSchemeResources(android.R.color.holo_blue_bright,
+                android.R.color.holo_green_light,
+                android.R.color.holo_orange_light,
+                android.R.color.holo_red_light);
+    }
+
+    public void fetchMovieAsync() {
 
         String url = "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed";
 
         AsyncHttpClient client = new AsyncHttpClient();
+
         client.get(url, new JsonHttpResponseHandler() {
             @Override
             public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
                 JSONArray movieJsonResults = null;
                 try {
+                    movieAdapter.clear();
                     movieJsonResults = response.getJSONArray("results");
                     movies.addAll( Movie.fromJSONArray(movieJsonResults) );
                     movieAdapter.notifyDataSetChanged();
+
+                    swipeContainer.setRefreshing(false);
 
                 } catch (JSONException e) {
                     e.printStackTrace();
